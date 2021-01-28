badba5111a30b2d5b6270ba0df3a6aa6081ba5c6
Glauber Ariel Magalhães <glaubermagalhaes@outlook.com>
2021-01-28 19:29:36 -0300

    criando os dados no javascript

--- js/scripts.js
+++ js/scripts.js
@@ -18,11 +18,35 @@ const Modal = {
     }
 }
 
+const transactions = [
+        {
+        id: 1,
+        description: 'Luz', 
+        amount: -50000,
+        date: '23/01/2021',
+        },
+        {
+        id: 2,
+        description: 'Website', 
+        amount: 50000,
+        date: '23/01/2021',
+        },
+        {
+        id: 3,
+        description: 'Internet', 
+        amount: -20000,
+        date: '23/01/2021',
+        },
+ ]
+
 const Transaction = { 
     incomes( ) {
         //somar as entradas
     },
     expenses(){ 
         //somar as saídas
+    },
+    total() {
+
     }
 }
