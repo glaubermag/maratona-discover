4008b2172fd430fdba14c8b93a894712f8d281ad
Glauber Ariel Magalhães <glaubermagalhaes@outlook.com>
2021-01-28 19:22:29 -0300

    convertendo o script para arquivo separado

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+const Modal = {
+    open(){
+        // Abrir modal
+        // Adicionar a class active ao modal
+        document
+            .querySelector('.modal-overlay')
+            .classList
+            .add('active')
+
+    },
+    close(){
+        // fechar o modal
+        // remover a class active do modal
+        document
+            .querySelector('.modal-overlay')
+            .classList
+            .remove('active')
+    }
+}
+
+const Transaction = { 
+    incomes( ) {
+        //somar as entradas
+    },
+    expenses(){ 
+        //somar as saídas
+    }
+}
